825795c027c34f3f2428a66a15ed20c656a1b612
Morten Omholt-Jensen <morten@omholt-jensen.com>
2020-01-19 14:12:16 +0100

    Initial Commit

---
+++ OpenGLProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLProject
+//
+//  Created by Morten Omholt-Jensen on 19/01/2020.
+//  Copyright © 2020 Morten Omholt-Jensen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
